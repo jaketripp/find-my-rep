894030d3958ca2060efbd27278194748ee4de172
Jacob Tripp <jake@Shimmer.domain>
2018-04-18 15:34:07 -0600

    Break Form into separate component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import './styles/default.css';
-import './styles/Form.css';
-import './styles/PersonList.css';
-import './styles/MoreInfo.css';
-import './styles/ErrorMessage.css';
+import "./styles/default.css";
+import "./styles/Form.css";
+import "./styles/PersonList.css";
+import "./styles/MoreInfo.css";
+import "./styles/ErrorMessage.css";
 
+import Form from "./components/Form";
 import MoreInfo from "./components/MoreInfo";
 import PersonList from "./components/PersonList";
 import ErrorMessage from "./components/ErrorMessage";
@@ -79,12 +80,6 @@ class App extends Component {
     this.setState(newState);
   };
 
-  partyAbbreviation = {
-    Republican: "R",
-    Democrat: "D",
-    Independent: "I"
-  };
-
   showMoreInfo = person => {
     let { state, party, name, district, phone, office, link } = person;
 
@@ -112,81 +107,12 @@ class App extends Component {
       <div className="App">
         <h1 className="blue">Who's My Representative?</h1>
         <div className="form">
-          <form onSubmit={this.submit}>
-            <select
-              name="position"
-              id="position"
-              value={this.state.position}
-              onChange={e => this.onSelectChange(e, "position")}
-              aria-label="Congress position"
-            >
-              <option value="">Select a Congress position</option>
-              <option value="Representatives">Representatives</option>
-              <option value="Senators">Senators</option>
-            </select>
-            <select
-              name="state"
-              id="state"
-              value={this.state.state}
-              onChange={e => this.onSelectChange(e, "state")}
-              aria-label="US State"
-            >
-              <option value="">Select a State</option>
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="AR">Arkansas</option>
-              <option value="CA">California</option>
-              <option value="CO">Colorado</option>
-              <option value="CT">Connecticut</option>
-              <option value="DE">Delaware</option>
-              <option value="FL">Florida</option>
-              <option value="GA">Georgia</option>
-              <option value="HI">Hawaii</option>
-              <option value="ID">Idaho</option>
-              <option value="IL">Illinois</option>
-              <option value="IN">Indiana</option>
-              <option value="IA">Iowa</option>
-              <option value="KS">Kansas</option>
-              <option value="KY">Kentucky</option>
-              <option value="LA">Louisiana</option>
-              <option value="ME">Maine</option>
-              <option value="MD">Maryland</option>
-              <option value="MA">Massachusetts</option>
-              <option value="MI">Michigan</option>
-              <option value="MN">Minnesota</option>
-              <option value="MS">Mississippi</option>
-              <option value="MO">Missouri</option>
-              <option value="MT">Montana</option>
-              <option value="NE">Nebraska</option>
-              <option value="NV">Nevada</option>
-              <option value="NH">New Hampshire</option>
-              <option value="NJ">New Jersey</option>
-              <option value="NM">New Mexico</option>
-              <option value="NY">New York</option>
-              <option value="NC">North Carolina</option>
-              <option value="ND">North Dakota</option>
-              <option value="OH">Ohio</option>
-              <option value="OK">Oklahoma</option>
-              <option value="OR">Oregon</option>
-              <option value="PA">Pennsylvania</option>
-              <option value="RI">Rhode Island</option>
-              <option value="SC">South Carolina</option>
-              <option value="SD">South Dakota</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VT">Vermont</option>
-              <option value="VA">Virginia</option>
-              <option value="WA">Washington</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
-            </select>
-            <button type="submit" className="submitBtn">
-              Submit
-            </button>
-          </form>
+          <Form
+            submit={this.submit}
+            onSelectChange={this.onSelectChange}
+            position={this.state.position}
+            state={this.state.state}
+          />
           {this.state.formError && (
             <ErrorMessage formError={this.state.formError} />
           )}
---
+++ src/components/Form.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+function Form(props) {
+  return (
+    <form onSubmit={props.submit}>
+      <select
+        name="position"
+        id="position"
+        value={props.position}
+        onChange={e => props.onSelectChange(e, "position")}
+        aria-label="Congress position"
+      >
+        <option value="">Select a Congress position</option>
+        <option value="Representatives">Representatives</option>
+        <option value="Senators">Senators</option>
+      </select>
+      <select
+        name="state"
+        id="state"
+        value={props.state}
+        onChange={e => props.onSelectChange(e, "state")}
+        aria-label="US State"
+      >
+        <option value="">Select a State</option>
+        <option value="AL">Alabama</option>
+        <option value="AK">Alaska</option>
+        <option value="AZ">Arizona</option>
+        <option value="AR">Arkansas</option>
+        <option value="CA">California</option>
+        <option value="CO">Colorado</option>
+        <option value="CT">Connecticut</option>
+        <option value="DE">Delaware</option>
+        <option value="FL">Florida</option>
+        <option value="GA">Georgia</option>
+        <option value="HI">Hawaii</option>
+        <option value="ID">Idaho</option>
+        <option value="IL">Illinois</option>
+        <option value="IN">Indiana</option>
+        <option value="IA">Iowa</option>
+        <option value="KS">Kansas</option>
+        <option value="KY">Kentucky</option>
+        <option value="LA">Louisiana</option>
+        <option value="ME">Maine</option>
+        <option value="MD">Maryland</option>
+        <option value="MA">Massachusetts</option>
+        <option value="MI">Michigan</option>
+        <option value="MN">Minnesota</option>
+        <option value="MS">Mississippi</option>
+        <option value="MO">Missouri</option>
+        <option value="MT">Montana</option>
+        <option value="NE">Nebraska</option>
+        <option value="NV">Nevada</option>
+        <option value="NH">New Hampshire</option>
+        <option value="NJ">New Jersey</option>
+        <option value="NM">New Mexico</option>
+        <option value="NY">New York</option>
+        <option value="NC">North Carolina</option>
+        <option value="ND">North Dakota</option>
+        <option value="OH">Ohio</option>
+        <option value="OK">Oklahoma</option>
+        <option value="OR">Oregon</option>
+        <option value="PA">Pennsylvania</option>
+        <option value="RI">Rhode Island</option>
+        <option value="SC">South Carolina</option>
+        <option value="SD">South Dakota</option>
+        <option value="TN">Tennessee</option>
+        <option value="TX">Texas</option>
+        <option value="UT">Utah</option>
+        <option value="VT">Vermont</option>
+        <option value="VA">Virginia</option>
+        <option value="WA">Washington</option>
+        <option value="WV">West Virginia</option>
+        <option value="WI">Wisconsin</option>
+        <option value="WY">Wyoming</option>
+      </select>
+      <button type="submit" className="submitBtn">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default Form;
